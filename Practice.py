1df4ca80374eadb980e561f834c5443c808a1794
YuvarajIcon <32995080+YuvarajIcon@users.noreply.github.com>
2020-02-01 13:26:05 +0530

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -1,16 +1,18 @@
+
+
 # -*- coding: utf-8 -*-
 import math
 import sys
 import unicodedata
 import random
 from random import randrange
-'''a=[int(x)for x in input().split()]
+a=[int(x)for x in input().split()]
 for i in range(len(a)):
     if a[i]%2!=0:
         a[i]=a[i]**2
-print(*a,sep=',')'''
+print(*a,sep=',')
 
-'''
+
 l = []
 s=0
 while True:
@@ -20,7 +22,6 @@ while True:
         l.append(line)
     else:
         break
-
 for i in range(0,len(l)):
     x=l[i]
     x=x.split()
@@ -30,9 +31,9 @@ for i in range(0,len(l)):
             s+=int(x[j+1])
         elif x[j]=='W':
             s-=int(x[j+1])
-print(s)'''
+print(s)
 
-'''def lcs(s1, s2):
+def lcs(s1, s2):
     matrix = [["" for x in range(len(s2))] for x in range(len(s1))]
     
     for i in range(len(s1)):
@@ -46,16 +47,12 @@ print(s)'''
                 #print(matrix[i-1][j])
                 #print( matrix[i][j-1])
                 matrix[i][j] = max(matrix[i-1][j], matrix[i][j-1], key=len)
-
     cs = matrix[-1][-1]
-
     return len(cs), cs
+print(lcs("", "bayxb"))
 
-print(lcs("", "bayxb"))'''
-
-'''ar=[str(x)for x in input().split(',')]
+ar=[str(x)for x in input().split(',')]
 c=0
-
 sm=0
 print(ar)
 for i in range(0,len(ar)):
@@ -66,7 +63,6 @@ for i in range(0,len(ar)):
     x=ar[i]
     if len(x)>=6 and len(x) <=12:
         for j in range(0,len(x)):
-
             if x[j].isalpha():
                 if x[j].islower():
                     l=1
@@ -78,8 +74,8 @@ for i in range(0,len(ar)):
                 sp=1
         
         if l==1 and u==1 and n==1 and sp==1:
-                print(x)'''
-'''l=[]
+                print(x)
+l=[]
 while True:
     line = input()
     if line:
@@ -87,22 +83,21 @@ while True:
     else:
         break
 l.sort()
-
 x=[]
 for i in range(0,len(l)):
     x.append(tuple(l[i].split(',')))
-print(x)'''
+print(x)
 
-'''class mine:
+class mine:
     def my_gen(x,y):
         for i in range(x,y+1):
             if i%7==0:
                 yield i
 for item in mine.my_gen(0,100):
-    print(item)'''
+    print(item)
 
 
-'''x1=0
+x1=0
 y1=0
 l1=[]
 x2=0
@@ -125,62 +120,62 @@ for j in range(0,len(l1)):
         if x[i]=="RIGHT":
             y2-=int(x[i+1])
 print(y2,x2)
-print(round(math.sqrt((x1-y2)**2+(y1-x2)**2))) '''
+print(round(math.sqrt((x1-y2)**2+(y1-x2)**2))) 
 
 
-'''a=input() 
+a=input() 
 w=[]
 a=a.split()
 res = {i : a.count(i) for i in set(a)}
 for i in sorted (res.keys()) :  
     w.append(i)
 for i in w :
-    print('{}:{}'.format(i,res[i]))'''
+    print('{}:{}'.format(i,res[i]))
 
 
 
-'''def square(n):
+def square(n):
     return n**2
-print(square(int(input())))'''
+print(square(int(input())))
 
-'''print(help(abs))
+print(help(abs))
 print(help(int))
-print(help(input))'''
+print(help(input))
 
-'''def my():
+def my():
     """fuuuuuuu"""
     return 0
-print(help(my))'''
+print(help(my))
 
-'''def i2s(n):
+def i2s(n):
     return str(n)
-print(i2s(int(input())))'''
+print(i2s(int(input())))
 
-'''class ff(object):
+class ff(object):
     def __init__(self,p):
         self.p=2
     def tt(self):
         return self.p
 a=ff(100)
-print(a.tt())'''
+print(a.tt())
 
-'''def ads(x,y):
+def ads(x,y):
     return int(x)+int(y)
-print(ads(input(),input()))'''
+print(ads(input(),input()))
 
-'''def con(x,y):
+def con(x,y):
     return x+y
-print(con(input(),input()))'''
+print(con(input(),input()))
 
-'''def high(s1,s2):
+def high(s1,s2):
     if len(s1)==len(s2):
         return s1,s2
     else:
         return max(s1,s2,key=len)
-print(high(input(),input()))'''
+print(high(input(),input()))
 
 
-'''def lc(s1,s2):
+def lc(s1,s2):
     c=0
     pp=0
     i=0
@@ -207,60 +202,58 @@ print(high(input(),input()))'''
                 print(s1[i:],s1[:j])
                 xx=len(s1[i:j])
         print(xx)
-
                 
     #pm=c if c>pp else pp   
     #print(s1[:pm])
     
-lc('pyopp;[','pooppop')'''
+lc('pyopp;[','pooppop')
 
 
 
-'''def pdict():
+def pdict():
     d={i : i**2 for i in range(0,21)}
     print(d)
+pdict()
 
-pdict()'''
-
-'''def dickeys():
+def dickeys():
     d1={i : i**2 for i in range(0,21)}
     print(d1.keys())
-dickeys()'''
+dickeys()
 
-'''def lis():
+def lis():
     l1=[i**2 for i in range(0,21)]
     print(l1)
-lis()'''
+lis()
 
-'''def lis1():
+def lis1():
     l1=[i**2 for i in range(0,21)]
     print(l1[:5])
-lis1()'''
+lis1()
 
-'''def lis2():
+def lis2():
     l1=[i**2 for i in range(0,21)]
     print(l1[-5:])
-lis2()'''
+lis2()
 
-'''def lis2():
+def lis2():
     l1=[i**2 for i in range(0,21)]
     print(l1[5:])
-lis2()'''
+lis2()
 
-'''def tp():
+def tp():
     l1=tuple(i**2 for i in range(0,21))
     print(l1)
-tp()'''
+tp()
 
-'''def htp(t):
+def htp(t):
     x=len(t)/2
     print(t[:int(x)])
     print(t[int(x):])
     
 t=[int(x)for x in input().split()]
-htp(tuple(t))'''
+htp(tuple(t))
 
-'''def etp(t):
+def etp(t):
     t1=[]
     for i in t:
         if i%2==0:
@@ -269,57 +262,56 @@ htp(tuple(t))'''
             
     
 t=[int(x)for x in input().split()]
-etp(t)'''
+etp(t)
 
-'''def yes(s):
+def yes(s):
     if s=="yes" or s=="YES" or s=="Yes":
         print("Yes")
     else:
         print("No")
-yes(input())'''
+yes(input())
 
-'''def sq(n):
+def sq(n):
     return n**2
 x=[int(x)for x in input().split()]
 l1=map(sq,x)
-print(list(l1))'''
+print(list(l1))
 
-'''def filt(n):
+def filt(n):
     if n%2==0:
         return True
     else:
         return False
 def ma(f):
     return f**2
-
 x=[int(x)for x in input().split()]
 p=filter(filt,x)
 l=map(ma,p)
-print(list(l))'''
+print(list(l))
 
-'''def fi(n):
+def fi(n):
     if n%2==0:
         return True
     else:
         return False
 x=[int(x) for x in input().split()]
-print(list(filter(fi,x)))'''
+print(list(filter(fi,x)))
 
-'''def kadane(a):
+def kadane(a):
     c=m=a[0]
     for i in a[1:]:
         c=max(c+i,i)
         m=max(c,m)
     return m
-print(kadane([int(x) for x in input().split()]))'''
+print(kadane([int(x) for x in input().split()]))
 
-'''class American:
+class American:
     @staticmethod
     def printNat():
         print("daaaa")
-American.printNat()'''
+American.printNat()
 
-'''class American:
+class American:
     
     def __init__(self):
         y=10
@@ -328,27 +320,27 @@ American.printNat()'''
 class NewYorker(American):
     def __init__(self):
         print("hii",super().__init__())
-a=NewYorker()'''
+a=NewYorker()
 
 
-'''class Circle:
+class Circle:
     def __init__(self,l):
         self.l=l
     def area(self,l):
         print(math.pi*self.l**2)
 c=Circle(10)
-c.area(c.l)'''
+c.area(c.l)
 
-'''class Rect:
+class Rect:
     def __init__(self,l,w):
         self.l=l
         self.w=w
     def area(self,l,w):
         print(l*w)
 c=Rect(10,20)
-c.area(c.l,c.w)'''
+c.area(c.l,c.w)
 
-'''class shape:
+class shape:
     def area(self):
         return 0
 class square(shape):
@@ -358,14 +350,14 @@ class square(shape):
         print(super().area())
         print(self.l*self.l)
 s=square(4)
-s.area(s.l)'''
+s.area(s.l)
 
-'''raise RuntimeError("raised")'''
-
-
+raise RuntimeError("raised")
 
 
-'''def minCost(cost, m, n): 
+
+
+def minCost(cost, m, n): 
     if (n < 0 or m < 0): 
         return sys.maxsize 
     elif (m == 0 and n == 0): 
@@ -377,12 +369,12 @@ s.area(s.l)'''
 cost= [ [1, 2, 3], 
         [4, 8, 2], 
         [1, 5, 3] ] 
-print(minCost(cost, 2, 2))'''
+print(minCost(cost, 2, 2))
 
-'''a=input()
-print(a[:a.find('@')])'''
+a=input()
+print(a[:a.find('@')])
 
-'''class Error(Exception):
+class Error(Exception):
     pass
 class aa(Error):
     pass
@@ -391,17 +383,17 @@ try:
    raise aa
   
 except aa:
-   print("duh")'''
+   print("duh")
 
-'''def a():
+def a():
    try:
        c=5/0
        print(a)
    except:
        print("duh")
-a()'''
+a()
 
-'''def count(S, m, n ): 
+def count(S, m, n ): 
     if (n == 0): 
         return 1
     if (n < 0): 
@@ -412,12 +404,12 @@ a()'''
 arr = [1, 2, 3] 
 m = len(arr) 
 print(count(arr, m, 4)) 
-'''
 
-'''a=input()
-print(a[a.find('@')+1:a.find('.')])'''
 
-'''a=[]
+a=input()
+print(a[a.find('@')+1:a.find('.')])
+
+a=[]
 b=input()
 b=b.split()
 try:
@@ -427,22 +419,20 @@ try:
             a.append(i)
 except:
     w=1
-print(a)'''
+print(a)
 
-'''a=u'\xeatre'
+a=u'\xeatre'
 b=unicodedata.normalize('NFKD', a).encode('ascii','ignore')
-print(b)'''
+print(b)
 
 
-'''n= int(input())
+n= int(input())
 a = 0
-
 for i in range(1,n+1):
     a = a+(i/(i+1))
+print(round(a,2))
 
-print(round(a,2))'''
-
-'''def f(n):
+def f(n):
     if n==0:
         return 0
     else:
@@ -451,9 +441,9 @@ xx=int(input())
 if xx==0:
     print('1')
 else:
-    print(f(xx))'''
+    print(f(xx))
 
-'''def fibo(n):
+def fibo(n):
         f=0
         f1=1
         f3=0
@@ -465,29 +455,29 @@ else:
             f1=f3
             
             print(f3)
-fibo(int(input()))'''
+fibo(int(input()))
 
 
-'''def gen(n):
+def gen(n):
     for i in range(0,n+1):
         if i%2==0:
             yield i
 x=[]
 for i in gen(10):
     x.append(i)
-print(*x,sep=',')'''
+print(*x,sep=',')
 
 
-'''def gen(n):
+def gen(n):
     for i in range(0,n+1):
         if i%5==0 and i%7==0:
             yield i
 x=[]
 for i in gen(100):
     x.append(i)
-print(*x,sep=',')'''
+print(*x,sep=',')
 
-'''f=0
+f=0
 a=[int(x) for x in input().split()]
 for i in a:
     try:
@@ -497,32 +487,32 @@ for i in a:
         print("no")
         
 if f==len(a):
-    print("yes")'''
+    print("yes")
 
 
-'''a=input()
-print(eval(a))'''
+a=input()
+print(eval(a))
 
-'''print (random.uniform(10,100))'''
+print (random.uniform(10,100))
 
-'''import random
-print(random.choice([i for i in range(11) if i%2==0]))'''
-
-
-'''import random
-print(random.choice([i for i in range(150) if i%5==0 and i%7==0]))'''
+import random
+print(random.choice([i for i in range(11) if i%2==0]))
 
 
-'''import random
-print (random.sample(range(100), 5))'''
+import random
+print(random.choice([i for i in range(150) if i%5==0 and i%7==0]))
 
-'''import random
-print (random.sample([i for i in range(100,201) if i%2==0], 5))'''
 
-'''import random
-print(random.sample([i for i in range(1,1001) if i%5==0 and i%7==0],5))'''
+import random
+print (random.sample(range(100), 5))
 
-'''def compress(string):
+import random
+print (random.sample([i for i in range(100,201) if i%2==0], 5))
+
+import random
+print(random.sample([i for i in range(1,1001) if i%5==0 and i%7==0],5))
+
+def compress(string):
     temp={}
     result=" "
     for x in string:
@@ -532,14 +522,9 @@ print(random.sample([i for i in range(1,1001) if i%5==0 and i%7==0],5))'''
             temp[x] = 1
     for key, value in temp.items():
         result += str(key) + str(value)
-
     return result
-
-
-
-
 s = input("Enter the string:")
-print(compress(s))'''
+print(compress(s))
    
 
 # coding: utf-8
@@ -812,15 +797,5 @@ for i in {k: v for k, v in sorted(res.items(), key=lambda item: item[1])}:
 
      
 
-
-
-
-
-
-
-
-
-
-        
 
 
